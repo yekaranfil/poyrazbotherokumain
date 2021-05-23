550474c1d9c8fc731195c4f6969d45e24782ad17
yekaranfil <77460386+yekaranfil@users.noreply.github.com>
2021-05-23 17:54:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const DiscordMusicBot = require("./structures/DiscordMusicBot");
 const client = new DiscordMusicBot();
-const db = require("quick.db")
 const Discord = require("discord.js")
-
-
 client.build();
 
-module.exports = client;({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+module.exports = client;
 
 // bot aktif olunca dm mesaj
-//client.on('ready', () => {
+client.on('ready', () => {
 
-//let id = "264142213833949184"; //Dmden Mesaj Yazılacak Kullanıcı ID
-//console.log("yazı") //Botunuz Yeniden Başlayınca Konsolunda Çıkan Yazı Örn: Bot hazır , aktif VB
-//client.users.cache.get(id).send("Komutlar Yüklendi Botunuz Aktif") 
-//});
-// bot aktif olunca dm mesaj bitiş
+    let id = "264142213833949184"; //Dmden Mesaj Yazılacak Kullanıcı ID
+    console.log("yazı") //Botunuz Yeniden Başlayınca Konsolunda Çıkan Yazı Örn: Bot hazır , aktif VB
+    client.users.cache.get(id).send("Komutlar Yüklendi Botunuz Aktif") 
+    });
+    // bot aktif olunca dm mesaj bitiş
 
-
-
-//------------------------------dm den hoşgeldin mesajı başlangıcı------------------------------
+//--------------------dm den hoşgeldin mesajı başlangıcı-----------------------
 //client.on("guildMemberAdd", guild => {
 //const embed = new Discord.MessageEmbed()
 //.setColor('BLUE')
@@ -28,22 +23,22 @@ module.exports = client;({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 //.setDescription('**Sunucuya hoşgeldin Lütfen kuralları okumayı unutma iyi eğlenceler! Genel sohbete yazı yazdığınızda ya da sesli odalarımıza girdiğinizde kuralları okumuş ve kabul etmiş sayılacaksınız.**')
 //.setImage("https://media.giphy.com/media/YOT0zQXXuBfcZkA1WU/giphy.gif")
 //.setTimestamp()
-//.setFooter("© 2020 Developed by EmreeK-Odinex🔸#5614", client.user.avatarURL())
+//.setFooter("© 2021 Developed by EmreeK-Odinex🔸#5614", client.user.avatarURL())
 //guild.send(embed)
 //})
-//------------------------------dm den hoşgeldin mesajı bitişi------------------------------
+//--------------------dm den hoşgeldin mesajı bitişi----------------------------
 
-//------------------------------ DM den bot eklenince mesaj komutu------------------------------
+// ----------------DM den bot eklenince mesaj komutu-----------------------
 //client.on("guildCreate", guild => {
 //  const embed = new Discord.MessageEmbed()
 //.setColor('RED')
 //.setAuthor(`${guild.name} Adlı Sunucuna ${client.user.username} başarıyla eklendi`,"https://media.giphy.com/media/lNLVInp4Tx3BLnR6Nx/giphy.gif")
 //.setThumbnail(client.user.avatarURL())
-//.setDescription(`**Beni sunucuna eklediğin için teşekkürler! Kullanım ve komutlar için \`.help\` veya \`.yardım\` yazman yeterli iyi eğlenceler!  
+//.setDescription(`**Beni sunucuna eklediğin için teşekkürler! Kullanım ve komutlar için \`*help\` veya \`*yardım\` yazman yeterli iyi eğlenceler!  
 
-//Thanks for adding me to the server! For usage and commands, just write \`.help\` or \`.yardım\` have fun!**`)
+//Thanks for adding me to the server! For usage and commands, just write \`*help\` or \`*yardım\` have fun!**`)
 //.setTimestamp()
-//.setFooter("© 2020 Developed by EmreeK-Odinex🔸#5614", client.user.avatarURL())
+//.setFooter("© 2021 Developed by EmreeK-Odinex🔸#5614", client.user.avatarURL())
 //.setImage("https://media.giphy.com/media/SWWR2NaACUu5qmf0Uo/giphy.gif")
 //.addField(`» \`Destek için EmreeK-Odinex🔸#5614\``," [Destek sunucusu]("+ client.config.SupportServer + ")");
 //guild.owner.send(embed)
@@ -52,9 +47,9 @@ module.exports = client;({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
   
  // console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 //});
- // DM eklenme bitiş
+ //---------------------------- DM eklenme bitiş------------------------------
 
-// ------------------------------yeni sunucuya eklenince rastgele mesaj ------------------------------
+// yeni sunucuya eklenince yardım mesaj 
 client.on('guildCreate', guild => {
   const channel = (guild.systemChannelID) 
   const kanal = guild.channels.cache.get(channel)
@@ -62,11 +57,11 @@ client.on('guildCreate', guild => {
   .setColor('RED')
 .setAuthor(`${guild.name} Adlı Sunucuya ${client.user.username} başarıyla eklendi`,"https://media.giphy.com/media/lNLVInp4Tx3BLnR6Nx/giphy.gif")
 .setThumbnail(client.user.avatarURL())
-.setDescription(`**Beni sunucuna eklediğin için teşekkürler! Kullanım ve komutlar için \`.help\` veya \`.yardım\` yazman yeterli iyi eğlenceler!  
+.setDescription(`**Beni sunucuna eklediğin için teşekkürler! Kullanım ve komutlar için \`*help\` veya \`*yardım\` yazman yeterli iyi eğlenceler!  
 
-Thanks for adding me to the server! For usage and commands, just write \`.help\` or \`.yardım\` have fun!**`)
+Thanks for adding me to the server! For usage and commands, just write \`*help\` or \`*yardım\` have fun!**`)
 .setTimestamp()
-.setFooter("© 2020 Developed by EmreeK-Odinex🔸#5614", client.user.avatarURL())
+.setFooter("© 2021 Developed by EmreeK-Odinex🔸#5614", client.user.avatarURL())
 .addField(`» \`Destek için EmreeK-Odinex🔸#5614\``," [Destek sunucusu]("+ client.config.SupportServer + ")");
 kanal.send(embed)
  
@@ -74,66 +69,66 @@ kanal.send(embed)
   
 
 });
-// ------------------------------yeni sunucuya eklenince rastgele mesaj bitiş------------------------------
+// yeni sunucuya eklenince yardım mesaj bitiş
 
-// ------------------------------Üyeye hoşgeldin mesajı ------------------------------
-//const guildInvites = new Map();
-//client.on('inviteCreate', async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
-//client.on('ready', () => {
-//    client.guilds.cache.forEach(guild => {
-//        guild.fetchInvites()
-//            .then(invites => guildInvites.set(guild.id, invites))
-//            .catch(err => console.log(err));
- //   });
-//});
+// Üyeye hoşgeldin mesajı 
+const guildInvites = new Map();
+client.on('inviteCreate', async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
+client.on('ready', () => {
+    client.guilds.cache.forEach(guild => {
+        guild.fetchInvites()
+            .then(invites => guildInvites.set(guild.id, invites))
+            .catch(err => console.log(err));
+    });
+});
 
-// client.on("guildMemberAdd", async (member) => {
-// const channel = (member.guild.systemChannelID); 
-// const cachedInvites = guildInvites.get(member.guild.id);
-// const newInvites = await member.guild.fetchInvites();
-// guildInvites.set(member.guild.id, newInvites);
-// try { 
-//        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses) || "1";
-//        const maze = new Discord.MessageEmbed()      
+ client.on("guildMemberAdd", async (member) => {
+ const channel = (member.guild.systemChannelID); 
+ const cachedInvites = guildInvites.get(member.guild.id);
+ const newInvites = await member.guild.fetchInvites();
+ guildInvites.set(member.guild.id, newInvites);
+ try { 
+        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses) || "1";
+        const maze = new Discord.MessageEmbed()      
       
-//.setColor("GREEN")
-//.setTitle(`<a:GiriGif:821076751760621620> Sunucuya yeni bir üye katıldı!`)
-//.setThumbnail(member.user.avatarURL)
-//.setDescription(`** \`${member.user.username}\` Sunucuya hoş geldin <a:ElSallamaGif:821076749164871680> seninle beraber `${member.guild.memberCount}\` kişiye ulaştık. **`)
-//         .addField(`:id: Üye ID:`, `${member.id}`, true)
-//.addField(` <a:raninbowp:821077131362566205> Katılan üye Adı`, `${member}`, true)
-//.addField(` <a:raninbowp:821077131362566205> Davet eden kişi`, `${usedInvite.inviter}`,true )
-//.addField(`Bu davet kodu ${usedInvite.uses} kere kullanılmış.`, `<a:Mee6LevelUpGif:821076739496738936><a:Mee6LevelUpGif:821076739496738936>`)
-//.setTimestamp()
-//.setFooter(`© 2021 ${client.user.username} Sayaç sistemi `, client.user.avatarURL())
-//client.channels.cache.get(channel).send(maze) 
-//  const welcomeChannel = member.guild.channels.cache.find(channel => channel.id === 'LOG KANALI ID');
- //      if(welcomeChannel) {
- //           welcomeChannel.send(maze).catch(err => console.log(err));
-//        }
-//    }
-//    catch(err) {
- //       console.log(err);
- //   }
-// });
-//------------------------------hoşgeldin bitiş------------------------------
+.setColor("GREEN")
+.setTitle(`<a:GiriGif:821076751760621620> Sunucuya yeni bir üye katıldı!`)
+.setThumbnail(member.user.avatarURL)
+.setDescription(`** \`${member.user.username}\` Sunucuya hoş geldin <a:ElSallamaGif:821076749164871680> seninle beraber \`${member.guild.memberCount}\` kişiye ulaştık. **`)
+ //        .addField(`:id: Üye ID:`, `${member.id}`, true)
+.addField(` <a:raninbowp:821077131362566205> Katılan üye Adı`, `${member}`, true)
+.addField(` <a:raninbowp:821077131362566205> Davet eden kişi`, `${usedInvite.inviter}`,true )
+.addField(`Bu davet kodu ${usedInvite.uses} kere kullanılmış.`, `<a:Mee6LevelUpGif:821076739496738936><a:Mee6LevelUpGif:821076739496738936>`)
+.setTimestamp()
+.setFooter(`© 2021 ${client.user.username} Sayaç sistemi `, client.user.avatarURL())
+client.channels.cache.get(channel).send(maze) 
+  const welcomeChannel = member.guild.channels.cache.find(channel => channel.id === 'LOG KANALI ID');
+       if(welcomeChannel) {
+            welcomeChannel.send(maze).catch(err => console.log(err));
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+ });
+//hoşgeldin bitiş
 
 // üyeye ayrılma mesajı
-//------------------------------ Üyeye hoşgeldin mesajı ------------------------------
-//client.on("guildMemberRemove", async member => {
-//const channel = (member.guild.systemChannelID) 
-//var maze = new Discord.MessageEmbed()
+client.on("guildMemberRemove", async member => {
+const channel = (member.guild.systemChannelID) 
+var maze = new Discord.MessageEmbed()
  
-//.setColor("RED")
-//.setTitle("<a:kGif:821076751761014844> Sunucudan ayrıldı!")
-//.setThumbnail(member.user.avatarURL)
-//.setDescription(`** \`${member.user.username}\` Sunucudan ayrıldı, sensiz \`${member.guild.memberCount}\`  kişiyiz :( **`)
+.setColor("RED")
+.setTitle("<a:kGif:821076751761014844> Sunucudan ayrıldı!")
+.setThumbnail(member.user.avatarURL)
+.setDescription(`** \`${member.user.username}\` Sunucudan ayrıldı, sensiz \`${member.guild.memberCount}\`  kişiyiz :( **`)
 //.addField(`:id: Üye ID:`, `${member.id}`, true)
-//.addField(`<a:raninbowp:821077131362566205> Üye Adı`, `${member}`, true)
-//.setFooter(`© 2021 ${client.user.username} Sayaç sistemi. `, client.user.avatarURL())
-//client.channels.cache.get(channel).send(maze) 
-//});
-//----------------------ayrılma bitiş------------------------------
+.addField(`<a:raninbowp:821077131362566205> Üye Adı`, `${member}`, true)
+.setTimestamp()
+.setFooter(`© 2021 ${client.user.username} Sayaç sistemi. `, client.user.avatarURL())
+client.channels.cache.get(channel).send(maze) 
+});
+//ayrılma bitiş
 
 // otorol 
 const qdb = require("quick.db")
@@ -148,19 +143,12 @@ client.on("guildMemberAdd", member => {
     }
   if (kanalcık == undefined) return
     const embedversion1mq = new Discord.MessageEmbed()
-    .setColor('BLACK')
+    .setColor('GREEN')
     .setAuthor(`${client.user.username} Otorol Sistemi`)
-    .setDescription(`
-    **${yazı}**
-    
-    **${member} kişisi ${member.guild} sunucusuna katıldı!**
-    
-    **Verilen rol: ${rolcük}**
-    
-    **Hoşgeldin ${member}! Seninle Birlikte ${member.guild.memberCount} kişi olduk!**
-    `)
+    .setDescription(`**${yazı} Verilen rol: ${rolcük}**  `)
 kanalcık.send(embedversion1mq)
     member.roles.add(rolcük.id)
 })
+
 
 //otorol bitiş 
