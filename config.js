64f97b239d96b80865cd14a47450fa396e1c4af6
yekaranfil <y.emrekaranfil@gmail.com>
2021-09-06 16:17:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ module.exports = {
   //Lavalink - Already there is a serer to connect :)
   Lavalink: {
     id: "main",
-    host: "lava.link",
-    port: 80,
-    pass: "80",
+    host: "fn.darrennathanael.com",
+    port: 2095,
+    pass: "whatwasthelastingyousaid",
     
   },
 
