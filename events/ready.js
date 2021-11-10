1ada6b1275d7a76b8a43fe8c7d66a92bdf702e25
yekaranfil <y.emrekaranfil@gmail.com>
2021-11-10 20:21:15 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,12 @@
 module.exports = async (client) => {
   client.Ready = true;
   
-  client.user.setActivity("YENİDEN BAŞLATILIYOR", { type: "PLAYING" }).then(() => {
+  client.user.setActivity("YENİDEN BAŞLATILIYOR...", { type: "PLAYING" }).then(() => {
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag);
     
-    let activities = [ `${client.guilds.cache.size} Sunucuda aktif!`, `${client.channels.cache.size} Kanalda!`, `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} kişi!`, `*help🔥|𝙀𝙢𝙧𝙚𝙆|™#1881` ], i = 0;
-    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "PLAYING" }), 2500);
+    let activities = [ `${client.guilds.cache.size} Sunucuda`, `${client.channels.cache.size} Kanalda!`, `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} kişi!`, `*help🛠️'Odinexia#1881` ], i = 0;
+    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "PLAYING" }), 2000);
   });
   client.RegisterSlashCommands()
 };
