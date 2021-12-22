71f4c0e18452644f9df2778e4c7d120ff3d966e2
yekaranfil <y.emrekaranfil@gmail.com>
2021-12-22 23:28:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ module.exports = {
   //Lavalink - Already there is a serer to connect :)
   Lavalink: {
     id: "main",
-    host: "linklava.cjstevenson.com",
-    port: 25503,
-    pass: "lookbehindyou",
+    host: "lava.pumpdev.org",
+    port: 3799,
+    pass: "pumpisfree",
     secure : false,
     
   },
