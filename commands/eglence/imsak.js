0334d2faa7e281fa9a6737961a6c19ba44cbedcf
yekaranfil <y.emrekaranfil@gmail.com>
2022-03-27 23:25:52 +0300

    s
    s

--- commands/eglence/imsak.js
+++ commands/eglence/imsak.js
@@ -15,16 +15,21 @@ module.exports = {
 
 run : async (client, message, args) => {
     const city = args[0];
+    var saat,dakika;
     if (!city) return message.channel.send('Şehir adı girmelisiniz.');
     axios.get(`https://api.collectapi.com/pray/single?ezan=%C4%B0msak&data.city=${city.toLowerCase()}`, {
         headers: {
             "content-type": "application/json",
             "authorization": "apikey 7gRVk42Dwd5XefesFuqSH8:0aXoQqqUmOFMxLYHUgFra9"
+            
         }
+        
     }).then(res => {
+        saat = `${res.data.result[0].hour}`;
+        dakika =`${res.data.result[0].min }`;
         const messageEmbed = new Discord.MessageEmbed().setDescription(`
            > **${city}** şehri için imsak saati **${res.data.result[0].time}.**
-           \`\`\`Kalan Süre: ${res.data.result[0].hour} ${res.data.result[0].min }\`\`\`
+           \`\`\`Kalan Süre: ${saat} ${dakika} \`\`\`
         `);
 
         message.channel.send(messageEmbed);
