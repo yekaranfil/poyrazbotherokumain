8c2420f35c0dd505b600d6ab56412e0495018b51
yekaranfil <y.emrekaranfil@gmail.com>
2022-03-28 17:56:41 +0300

    Update iftar.js

--- commands/eglence/iftar.js
+++ commands/eglence/iftar.js
@@ -14,7 +14,20 @@ module.exports = {
   aliases: ["İftar","akşam","İftar"],
 
 run : async (client, message, args) => {
+    var saat,sayac, imsak;
+    var kalansaat, kalandakika;
+    let date_ob = new Date();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    console.log(hours + ":" + minutes);
+
+    saat = hours +3;
+    //if(saat < 9) {
+      //  saat = saat-24;
+    //} 
+    
     const city = args[0];
+
     if (!city) return message.channel.send('Şehir adı girmelisiniz.');
     axios.get(`https://api.collectapi.com/pray/single?ezan=Ak%C5%9Fam&data.city=${city.toLowerCase()}`, {
         headers: {
@@ -22,9 +35,29 @@ run : async (client, message, args) => {
             "authorization": "apikey 1iysmakoIybrglVCfZkbDS:10KxqIwpFNuA0Q12fkHZge"
         }
     }).then(res => {
+
+        iftar = res.data.result[0].time;
+
+        var saat1 = iftar.split(":");
+        var saatData = saat1[0];
+        var dakikaData = saat1[1];
+        saatData = saatData.charAt(1);
+
+        kalansaat= parseInt(saatData)-parseInt(saat);
+    
+        if (dakikaData < minutes ) {
+            kalandakika = 60 - parseInt(minutes);
+            kalandakika = parseInt(dakikaData) + parseInt(kalandakika);
+            kalansaat = parseInt(kalansaat) -1;
+        } else {
+             kalandakika = parseInt(dakikaData) - parseInt(minutes);
+             
+        }
+        var sonsaat = Math.abs(kalansaat);
+
         const messageEmbed = new Discord.MessageEmbed().setDescription(`
-           > **${city}** şehri için iftar saati **${res.data.result[0].time}.**
-           \`\`\`Kalan Süre: ${res.data.result[0].hour} ${res.data.result[0].min}\`\`\`
+           > **${city}** şehri için iftar saati **${res.data.result[0].time} - Şuan ki saat: ${saat}:${minutes}.**
+           \`\`\`İftar Vaktine Kalan Süre: ${sonsaat} Saat ${kalandakika} Dakika.\`\`\`
         `);
 
         message.channel.send(messageEmbed);
